4966326cc7fac3085e1d8ed7b4eafdb3d9aca137
Ryan <pwnzlps@gmail.com>
2018-02-12 14:55:36 -0500

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,88 @@
+//group0
+var strength = 1,
+    perception = 1,
+    endurance = 1,
+    charisma = 1,
+    intelligence = 1,
+    agility = 1,
+    luck = 1;
+    
+var ironFist = 0, //group1   5-4-5-3-2-5-4
+    pickpocket = 0,
+    toughness = 0,
+    capCollector = 0,
+    vans = 0,
+    gunslinger = 0,
+    fortuneFinder = 0;
+
+var bigLeagues = 0, //group2   5-5-3-3-4-5-4
+    rifleman = 0,
+    leadBelly = 0,
+    ladyKiller = 0,
+    medic = 0,
+    commando = 0,
+    scrounger = 0;
+    
+var armorer = 0, //group3   4-2-3-4-4-5-4
+    awareness = 0,
+    lifegiver = 0,
+    loneWanderer = 0,
+    gunNut = 0,
+    sneak = 0,
+    bloodyMess = 0;
+    
+var blacksmith = 0, //group4   3-4-2-4-4-3-4
+    locksmith = 0,
+    chemResistant = 0,
+    attackDog = 0,
+    hacker = 0,
+    misterSandman = 0,
+    mysteriousStranger = 0;
+    
+var heavyGunner = 0, //group5   5-4-2-3-3-3-3
+    demoExpert = 0,
+    aquaboy = 0,
+    animalFriend = 0,
+    scrapper = 0,
+    actionBoy = 0,
+    idiotSavant = 0;
+    
+var strongBack = 0, //group6   0-3-4-2-4-3-3
+    nightPerson = 0,
+    radResistant = 0,
+    localLeader = 0,
+    science = 0,
+    movingTarget = 0,
+    betterCriticals = 0;
+    
+var steadyAim = 0, //group7   3-5-3-3-4-3-4
+    refractor = 0,
+    adamSkel = 0,
+    partyBoy = 0,
+    chemist = 0,
+    ninja = 0,
+    critBank = 0;
+
+var basher = 0, //group8   4-3-3-3-3-3-3
+    sniper = 0,
+    cannibal = 0,
+    inspirational = 0,
+    roboticsExpert = 0,
+    quickHands = 0,
+    grimReapSpr = 0;
+
+var rooted = 0, //group9   3-2-4-3-3-2-4
+    penetrator = 0,
+    ghoulish = 0,
+    wstlWisp = 0,
+    nucPhys = 0,
+    blitz = 0,
+    fLC = 0;
+
+var painTrain = 0, //group10   3-3-3-3-3-3-3
+    concFire = 0,
+    solarPowered = 0,
+    intimidation = 0,
+    nerdRage = 0,
+    gunFu = 0,
+    ricochet = 0;
